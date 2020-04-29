4c83151199fbd128eb1f3ca49591d527b4a7bf97
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-29 16:45:41 +0200

    fixed esercizio dopo il ticket

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-$('i').click(function functionName() {
-$('hamburger-menu').show();
+$('.header-right > a').click(function() {
+// $('.hamburger-menu').show();metodi 1
+$('.hamburger-menu').addClass("active");
+})
+
+$('.hamburger-menu > a').click(function() {
+// $('.hamburger-menu').hide();metodo 1
+$('.hamburger-menu').removeClass("active");
 })
